591be887e03f6ebaf624c3ed9151713b7efa98f7
Thananjaya <thananjaya091993@gmail.com>
2019-02-15 15:19:21 +0530

    minor changes in deployment files

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
-const truffleProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode} = require('./compile');
+const truffleProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
 const provider = new truffleProvider(
-  'open pride vacant cushion soccer shadow eager omit robust soft armed loan',
-  'https://rinkeby.infura.io/v3/ffd2deb0df864ae299640e5d442c4eaa'
+  "open pride vacant cushion soccer shadow eager omit robust soft armed loan",
+  "https://rinkeby.infura.io/v3/ffd2deb0df864ae299640e5d442c4eaa"
 );
 
 const web3 = new Web3(provider);
@@ -12,7 +12,7 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ from: accounts[0], gas: '1000000' });
-}
+    .send({ from: accounts[0], gas: "1000000" });
+};
 
 deploy();
